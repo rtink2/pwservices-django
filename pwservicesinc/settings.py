3d3616a4e37966fce6aee10e9d31a13f6eb85a65
rtink2 <tinkham0408@gmail.com>
2021-08-02 17:13:38 -0400

    updated code

--- pwservicesinc/settings.py
+++ pwservicesinc/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -161,4 +160,5 @@ PHONENUMBER_DEFAULT_FORMAT= 'E164'
 TIME_INPUT_FORMATS = ('%I:%M %p',)
 
 # Activate Django-Heroku.
+import django_heroku
 django_heroku.settings(locals())
