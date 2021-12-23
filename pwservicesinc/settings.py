d5e12dc8aab57889c12459bb89339256b8db0096
rtink2 <tinkham0408@gmail.com>
2021-12-23 07:48:34 -0500

    update settings

--- pwservicesinc/settings.py
+++ pwservicesinc/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
